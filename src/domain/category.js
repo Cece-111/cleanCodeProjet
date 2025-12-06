efdd7501775147d62bb38fb4c9c3db2926d66f2e
Cece111 <cv.04@laposte.net>
2025-12-06 13:15:00 +0100

    Ajout de la logique des categories Leitner

---
+++ src/domain/category.js
@@ -0,0 +1,28 @@
+export const Category = {
+    FIRST: 'FIRST',
+    SECOND: 'SECOND',
+    THIRD: 'THIRD',
+    FOURTH: 'FOURTH',
+    FIFTH: 'FIFTH',
+    SIXTH: 'SIXTH',
+    SEVENTH: 'SEVENTH',
+    DONE: 'DONE',
+};
+
+export const Intervals = {
+    [Category.FIRST]: 1,
+    [Category.SECOND]: 2,
+    [Category.THIRD]: 4,
+    [Category.FOURTH]: 8,
+    [Category.FIFTH]: 16,
+    [Category.SIXTH]: 32,
+    [Category.SEVENTH]: 64,
+};
+
+export const getNextCategory = (currentCategory) => {
+    const categories = Object.values(Category);
+    const index = categories.indexOf(currentCategory);
+    if (index === -1 || currentCategory === Category.DONE) return Category.DONE;
+    if (index === categories.length - 2) return Category.DONE; // After SEVENTH comes DONE
+    return categories[index + 1];
+};
